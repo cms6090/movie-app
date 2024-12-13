cb56977ab2e40696d0f98513181046ee31e4c714
MinSeokCho <cho03man2@naver.com>
2024-12-13 16:43:14 +0900

    예약(잔여석, 잔여좌석)

--- src/index.js
+++ src/index.js
@@ -8,7 +8,11 @@ import { setMovieList } from "./redux/movieSlice";
 import { collection, getDocs } from "firebase/firestore";
 import { db } from "./firebaseinit";
 
+let dataFetched = false; // 데이터가 이미 로드되었는지 확인하는 플래그
+
 const fetchData = async () => {
+  if (dataFetched) return; // 이미 데이터를 가져왔으면 실행하지 않음
+
   try {
     // Firestore에서 특별관 데이터 가져오기
     const theaterSnapshot = await getDocs(collection(db, "theater"));
@@ -26,8 +30,7 @@ const fetchData = async () => {
     }));
     store.dispatch(setMovieList(movies));
 
-    console.log("Theaters:", theaters);
-    console.log("Movies:", movies);
+    dataFetched = true; // 데이터 로드 상태 업데이트
   } catch (error) {
     console.error("Error fetching initial data:", error);
   }
